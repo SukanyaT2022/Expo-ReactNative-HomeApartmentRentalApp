bacf9b4a7998841567c4b07bbce3b3890b2f72c1
TiparpronS. <96738451+SukanyaT2022@users.noreply.github.com>
2026-02-06 09:35:39 -0500

    add unterface user component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,6 @@
 
 import UserOwnerBarComp from "@/components/UserOwnerBarComp";
+import { AntDesign } from "@expo/vector-icons";
 import { Image } from "expo-image";
 import { Platform, StyleSheet, Text, View } from "react-native";
 
@@ -7,7 +8,29 @@ export default function HomeScreen() {
   return (
     <View style={styles.container}>
       <Text>Home Screen</Text>
-      <UserOwnerBarComp />
+      <UserOwnerBarComp 
+      // imagePropOnly={require("@/assets/images/dog.jpg")}
+        imageUrlProp="https://images.unsplash.com/photo-1533738363-b7f9aef128ce?q=80&w=1035&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        titleProp="John Doe"
+        subtitleProp="Premium Member"
+        // iconProps={ <AntDesign name="bell" size={24} color="black" />}
+        iconProps={
+<View style={styles.iconParentWrap}>
+<View style={styles.iconcontainer}>
+
+  <View style={styles.wrapIcon}></View>
+  <AntDesign name="bell" size={24} color="black" />
+</View>
+
+{/* <View style={styles.iconcontainer}>
+  <View style={styles.wrapIcon}></View>
+  <AntDesign name="bell" size={24} color="black" />
+</View> */}
+
+</View>
+        }
+      />
+      
     </View>
   );
 }
@@ -16,4 +39,18 @@ const styles = StyleSheet.create({
   container: {
     padding: 40,
   },
+   iconcontainer: {
+    width: 40,
+    height: 40,
+    backgroundColor: "#C0C0C0",
+    borderRadius: 100,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  wrapIcon: {},
+  iconParentWrap:{
+    flexDirection: "row",
+    alignContent: "center",
+    gap:8,
+  }
 });
--- components/userOwnerBarComp.tsx
+++ components/userOwnerBarComp.tsx
@@ -1,63 +1,82 @@
-import { Image, StyleSheet, Text, View } from 'react-native'
-import React, { use } from 'react'
-import { Ionicons, AntDesign} from '@expo/vector-icons'
+import { Image, StyleSheet, Text, View } from "react-native";
+import React, { ReactNode, use } from "react";
+import { Ionicons, AntDesign } from "@expo/vector-icons";
 interface UserOwnerBarCompProps {
-
+  // imagePropOnly?: string;
+  imageUrlProp?: string;
+  titleProp?: string;
+  subtitleProp?: string;
+  iconProps?: ReactNode;
 }
-const UserOwnerBarComp = () => {
+const UserOwnerBarComp = ({
+  // imagePropOnly,
+  imageUrlProp,
+  titleProp,
+  subtitleProp,
+  iconProps,
+}: UserOwnerBarCompProps) => {
   return (
-    <View  style={styles.container}>
-<View style={styles.wrapImgtext}>
-     <Image
-        source={require('@/assets/images/dog.jpg')}
-        style={styles.userImage}
-      />
-      <View style={styles.textcontainer}>
-      <Text>userOwnderBarComp</Text>
-      <Text>userOwnderBarComp</Text>
+    <View style={styles.container}>
+      
+      <View style={styles.wrapImgtext}>
+         {/* <Image
+          source={
+            imagePropOnly ? imagePropOnly :require("@/assets/images/dog.jpg")
+          }
+          style={styles.userImage}
+        /> */}
+        <Image
+          source={
+            imageUrlProp
+              ? { uri: imageUrlProp }
+              : require("@/assets/images/dog.jpg")
+          }
+          style={styles.userImage}
+        />
+        <View style={styles.textcontainer}>
+          <Text style={styles.title}>{titleProp}</Text>
+          <Text style={styles.subtitle}>{subtitleProp}</Text>
+        </View>
       </View>
-</View>
-<View style={styles.iconcontainer}>
-  <View style={styles.wrapIcon}></View>
-  <AntDesign name="bell" size={24} color="black" />
-</View>
+   { iconProps}
     </View>
-  )
-}
+  );
+};
 
-export default UserOwnerBarComp
+export default UserOwnerBarComp;
 
 const styles = StyleSheet.create({
-    container:{
-      display: 'flex',
-      flexDirection: 'row',
-       justifyContent: 'space-between',
-   
-    },
-    iconcontainer:{
-width: 50,  
-height: 50,
-backgroundColor: 'gray',
-borderRadius: 100,
-justifyContent: 'center',
-alignItems: 'center',
-    },
-    wrapImgtext:{
-       flexDirection: 'row',
+  container: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  title: {
+    fontSize: 18,
+  },
+  subtitle: {
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  iconcontainer: {
+    width: 50,
+    height: 50,
+    backgroundColor: "#C0C0C0",
+    borderRadius: 100,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  wrapImgtext: {
+    flexDirection: "row",
     // justifyContent: 'space-between',
-      gap: 15,
-          alignItems: 'center',
-    },
-     textcontainer:{
-
-    },
-    userImage: {    
-      width: 30,
-      height: 30,
-      borderRadius: 25,
-    },
-    wrapIcon:{
-
-    },
-
-})
+    gap: 15,
+    alignItems: "center",
+  },
+  textcontainer: {},
+  userImage: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+  },
+  wrapIcon: {},
+});
